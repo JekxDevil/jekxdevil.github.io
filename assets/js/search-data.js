427d8dc624121e6d1af7d64927a9ccf59e265120
JekxDevil <jekx123devil@gmail.com>
2025-03-30 18:08:57 +0000

    Deploying to gh-pages from @ JekxDevil/jekxdevil.github.io@b3b6f0a8364d633c7f1263f543544780f73875a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "The resume I use for applications is available to download by clicking on the pdf button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
